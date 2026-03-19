ea1dbd41af4e067d30407f58b96b2b4437f19bfb
TAREMWA-BENJAMIN <taremwabenja.jb@gmail.com>
2026-03-19 14:21:54 +0300

    loging section page

--- core/resources/views/templates/basic/user/auth/login.blade.php
+++ core/resources/views/templates/basic/user/auth/login.blade.php
@@ -15,7 +15,7 @@
                         {{ __(@$loginContent->data_values->heading) }}
                     </h3>
 
-                    <div class="login-info-box">
+                    <div class="login-info-box mb-5">
                         <p class="login-info-text mb-3">
                             @php
                                 echo __(strip_tags(@$loginContent->data_values->intro_1 ?? "Ready to start your language learning journey? Applying to Lana Language Centre is simple and straightforward. Whether you want to learn German or explore Ugandan languages, we're here to guide you through the process."));
@@ -42,6 +42,43 @@
                                     <li>▢ {{ __(@$loginContent->data_values->step_6 ?? 'Last Decision') }}</li>
                                 </ul>
                             </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="login-extra-box">
+                    <div class="row g-4">
+                        <div class="col-md-6">
+                            <h5 class="login-extra-heading mb-3">
+                                {{ __(@$loginContent->data_values->know_first_title ?? 'Things To Know First') }}
+                            </h5>
+                            <p class="login-info-text mb-0">
+                                @php
+                                    echo __(strip_tags(@$loginContent->data_values->know_first_body ?? 'Before applying, please review our course offerings and determine which language and level best suits your needs. We offer courses in German (A1 to C1 levels) and various Ugandan languages including Luganda, Acholi, Runyoro, and more.<br><br>All applicants should have a genuine interest in language learning and be committed to attending classes regularly. No prior language experience is required for beginner courses. We welcome students of all ages and backgrounds.'));
+                                @endphp
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+                            <h5 class="login-extra-heading mb-3">
+                                {{ __(@$loginContent->data_values->required_docs_title ?? 'Required Documents') }}
+                            </h5>
+                            <p class="login-info-text">
+                                @php
+                                    echo __(strip_tags(@$loginContent->data_values->required_docs_intro ?? "To complete your application, you'll need to provide the following documents and information:"));
+                                @endphp
+                            </p>
+                            <ul class="list-unstyled login-steps mb-0">
+                                <li>▢ {{ __(@$loginContent->data_values->required_doc_1 ?? 'Completed application form (available online or at our office)') }}</li>
+                                <li>▢ {{ __(@$loginContent->data_values->required_doc_2 ?? 'Copy of national ID or passport') }}</li>
+                                <li>▢ {{ __(@$loginContent->data_values->required_doc_3 ?? 'Two recent passport-size photographs') }}</li>
+                                <li>▢ {{ __(@$loginContent->data_values->required_doc_4 ?? 'Proof of payment or payment plan agreement') }}</li>
+                                <li>▢ {{ __(@$loginContent->data_values->required_doc_5 ?? 'For advanced courses, proof of previous language level (if applicable)') }}</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
